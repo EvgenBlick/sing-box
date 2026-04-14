41849e532627987f2919a7b6f6e56910bab2d734
rosubovandrej-star <rosubovandrej@gmail.com>
2026-04-14 14:24:34 +0300

    fix(libbox): remove unstable pidfd linkname on android

--- experimental/libbox/pidfd_android.go
+++ experimental/libbox/pidfd_android.go
@@ -1,17 +1,15 @@
-//go:build android && !go1.26
+//go:build android
 
 package libbox
 
 import (
 	"os"
-	_ "unsafe"
 )
 
 // https://github.com/SagerNet/sing-box/issues/3233
 // https://github.com/golang/go/issues/70508
 // https://github.com/tailscale/tailscale/issues/13452
 
-//go:linkname checkPidfdOnce os.checkPidfdOnce
 var checkPidfdOnce func() error
 
 func init() {
--- experimental/libbox/pidfd_android_go126.go
+++
@@ -1,9 +0,0 @@
-//go:build android && go1.26
-
-package libbox
-
-import "os"
-
-func init() {
-	_ = os.ErrInvalid
-}
